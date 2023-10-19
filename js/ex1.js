394009de713353ed396190f29032cf5b6314a83a
Mattias <mattias.franklin2@gmail.com>
2023-10-19 16:04:49 -0700

    Laptop to Github

---
+++ js/ex1.js
@@ -0,0 +1,16 @@
+const questions = [
+    {
+      statement: "2+2?",
+      answer: "2+2 = 4"
+    },
+    {
+      statement: "In which year did Christopher Columbus discover America?",
+      answer: "1492"
+    },
+    {
+      statement:
+        "What occurs twice in a lifetime, but once in every year, twice in a week but never in a day?",
+      answer: "The E letter"
+    }
+  ];
+  
---
+++ js/ex3.js
@@ -0,0 +1,17 @@
+// Country list
+const countryList = [
+    "Afghanistan",
+    "Albania",
+    "Algeria",
+    "Andorra",
+    "Angola",
+    "Anguilla",
+    "Antarctica",
+    "Antigua-and-Barbuda",
+    "Argentina",
+    "Armenia",
+    "Aruba",
+    "Australia",
+    "Autria",
+    "Azerbaïjan"
+  ];  
